ffbf40ef8400ee4c213d64ef26ee2b18fb3f4d8c
Jamova01 <jorgemova01@gmail.com>
2023-06-08 17:25:09 -0500

    [UPDATE]: Impmenting setProductToShow

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,20 +1,35 @@
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../context'
+import { PlusIcon } from '@heroicons/react/24/solid'
 
 const Card = ({ data }) => {
-    const { count, setCount } = useContext(ShoppingCartContext)
+    const {
+        count,
+        setCount,
+        openProductDetail,
+        setProductToShow
+    } = useContext(ShoppingCartContext)
+
+    const showProduct = (productDetail) => {
+        openProductDetail()
+        setProductToShow(productDetail)
+    }
+
     return (
-        <div className='bg-white cursor-pointer w-56 h-60'>
+        <div
+            className='bg-white cursor-pointer w-56 h-60'
+            onClick={() => showProduct(data)}
+        >
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>
                     {data.category.name}
                 </span>
-                <img className='w-full h-full object-cover rounded-lg' src={data.images[0]} alt={data.title} />
+                <img className='w-full h-full object-cover rounded-lg' src={data.images} alt={data.title} />
                 <div
                     className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
                     onClick={() => setCount(count + 1)}
                 >
-                    +
+                    <PlusIcon className='h-6 w-6 text-black' />
                 </div>
             </figure>
             <p className='flex justify-between'>
