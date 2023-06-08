533adc290a693280d9f4043c1be9190f0a1059ad
Jamova01 <jorgemova01@gmail.com>
2023-06-08 17:27:32 -0500

    [UPDATE]: Implementing product detail

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -3,12 +3,27 @@ import { createContext, useState } from 'react'
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartPrivider = ({ children }) => {
+    // Increment quantity
     const [count, setCount] = useState(0)
+
+    // Product Detail - Open/Close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    // Product Detail - Show product
+    const [productToShow, setProductToShow] = useState({})
+
     return (
         <ShoppingCartContext.Provider value={
             {
                 count,
-                setCount
+                setCount,
+                openProductDetail,
+                closeProductDetail,
+                isProductDetailOpen,
+                productToShow,
+                setProductToShow
             }
         }>
             {children}
