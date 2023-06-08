615a2826fd656c3466b4707dd4202e6b3488f760
Jamova01 <jorgemova01@gmail.com>
2023-06-08 10:20:53 -0500

    [UPDATE]: Making a GET request

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react'
+import Card from '../../components/Card'
 import Layout from '../../components/Layout'
 
 const Home = () => {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+    }, [])
 
     return (
         <Layout>
             Home
+            <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+                {
+                    items?.map(product => (
+                        <Card key={product.id} data={product} />
+                    ))
+                }
+            </div>
+
         </Layout>
     )
 }
