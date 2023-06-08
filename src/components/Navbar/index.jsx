bc1489662170012698db73127201c18f1bdfc9d0
Jamova01 <jorgemova01@gmail.com>
2023-06-08 12:08:34 -0500

    [UPDATE]: Implementing ShoppingCartContext

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,10 @@
-import { NavLink } from 'react-router-dom'
+import { useContext } from 'react';
+import { NavLink } from 'react-router-dom' 
+import { ShoppingCartContext } from '../../context';
 
 const Navbar = () => {
-    const activeStyle = "underline underline-offset-4";
+    const { count } = useContext(ShoppingCartContext)
+    const activeStyle = 'underline underline-offset-4';
 
     return (
         <nav className='flex justify-between items-center fixed top-0 z-10 w-full py-5 px-8 text-sm font-light'>
@@ -110,7 +113,7 @@ const Navbar = () => {
                     </NavLink>
                 </li>
                 <li>
-                    0
+                    {count}
                 </li>
             </ul>
         </nav>
