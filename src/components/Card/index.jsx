caa30028eea32fd7dd9a1455cb40845659a8d9dd
Jamova01 <jorgemova01@gmail.com>
2023-06-11 15:32:30 -0500

    [UPDATE]: Implementing render icon function

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../context'
-import { PlusIcon } from '@heroicons/react/24/solid'
+import { PlusIcon, CheckIcon } from '@heroicons/react/24/solid'
 
 const Card = ({ data }) => {
     const {
@@ -32,6 +32,30 @@ const Card = ({ data }) => {
         console.log('CART:', cartProducts);
     }
 
+    const renderIcon = (id) => {
+        const isInCart = cartProducts.filter(product => product.id === id).length > 0
+
+        if (isInCart) {
+            return (
+                <div
+                    className='absolute top-0 right-0 flex justify-center items-center bg-black w-6 h-6 rounded-full m-2 p-1'
+                >
+                    <CheckIcon className='h-6 w-6 text-white' />
+                </div>
+            )
+        } else {
+            return (
+                <div
+                    className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
+                    onClick={(event) => addProductsToCart(event, data)}
+                >
+                    <PlusIcon className='h-6 w-6 text-black' />
+                </div>
+            )
+        }
+
+    }
+
     return (
         <div
             className='bg-white cursor-pointer w-56 h-60'
@@ -42,12 +66,7 @@ const Card = ({ data }) => {
                     {data.category.name}
                 </span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.images} alt={data.title} />
-                <div
-                    className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-                    onClick={(event) => addProductsToCart(event, data)}
-                >
-                    <PlusIcon className='h-6 w-6 text-black' />
-                </div>
+                {renderIcon(data.id)}
             </figure>
             <p className='flex justify-between'>
                 <span className='text-sm font-light'>{data.title}</span>
