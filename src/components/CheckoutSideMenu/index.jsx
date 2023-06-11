6d2736f990593b72d168ab1a8462452991fb51cf
Jamova01 <jorgemova01@gmail.com>
2023-06-11 15:30:11 -0500

    [UPDATE]: Implementing cart products

--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -1,13 +1,17 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../context'
+import OrderCard from '../OrderCard'
 
 const CheckoutSideMenu = () => {
-    const {isCheckoutSideMenuOpen, closeCheckoutSideMenu } = useContext(ShoppingCartContext)
+    const {
+        isCheckoutSideMenuOpen,
+        closeCheckoutSideMenu,
+        cartProducts } = useContext(ShoppingCartContext)
 
     return (
         <aside
-        className={`${isCheckoutSideMenuOpen ? 'flex' : 'hidden'} w-[360px] h-[calc(100vh-68px)] top-[68px] flex-col fixed right-0 border border-black rounded-lg bg-white`}
+            className={`${isCheckoutSideMenuOpen ? 'flex' : 'hidden'} w-[360px] h-[calc(100vh-68px)] top-[68px] flex-col fixed right-0 border border-black rounded-lg bg-white`}
         >
             <div className='flex justify-between items-center'>
                 <h2 className='font-medium text-xl p-4'>My order</h2>
@@ -17,6 +21,19 @@ const CheckoutSideMenu = () => {
                     <XMarkIcon className='h-6 w-6 text-black cursor-pointer' />
                 </div>
             </div>
+            <div className='flex flex-col overflow-y-scroll gap-4 px-6'>
+                {
+                    cartProducts.map(product => (
+                        <OrderCard
+                            key={product.id}
+                            title={product.title}
+                            imageUrl={product.images}
+                            price={product.price}
+                        />
+                    ))
+                }
+            </div>
+
         </aside>
     )
 }
