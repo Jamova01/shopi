bccccb7d838115bbaba6d4ee25946612d45dce32
Jamova01 <jorgemova01@gmail.com>
2023-06-12 15:29:16 -0500

    [UPDATE]: Implementing conditional render of XMarkIcon

--- src/components/OrderCard/index.jsx
+++ src/components/OrderCard/index.jsx
@@ -3,6 +3,15 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 const OrderCard = (props) => {
     const { id, title, imageUrl, price, handleDelete } = props
 
+    let renderXMarkIcon
+
+    if (handleDelete) {
+        renderXMarkIcon = <XMarkIcon
+            className='h-6 w-6 text-black cursor-pointer'
+            onClick={() => handleDelete(id)}
+        />
+    }
+
     return (
         <div className='flex justify-between items-center'>
             <div className='flex items-center gap-2'>
@@ -14,11 +23,11 @@ const OrderCard = (props) => {
 
             <div>
                 <p className='text-lg font-medium'>{price}</p>
-                <XMarkIcon
-                    className='h-6 w-6 text-black cursor-pointer'
-                    onClick={() => handleDelete(id)}
-                />
+
             </div>
+            {
+                renderXMarkIcon
+            }
         </div>
     )
 }
