4930b100984fe9d102c26d4ed7be3743517b8e04
Jamova01 <jorgemova01@gmail.com>
2023-06-12 15:24:12 -0500

    [UPDATE]: Implementing OrderCard

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,9 +1,31 @@
+import { useContext } from 'react'
+
+// Context
+import { ShoppingCartContext } from '../../context'
+
+// Components
 import Layout from '../../components/Layout'
+import OrderCard from '../../components/OrderCard'
 
 function MyOrder() {
+    const { order } = useContext(ShoppingCartContext)
+
     return (
         <Layout>
             MyOrder
+            <div className='flex flex-col w-80 gap-4'>
+                {
+                    order?.slice(-1)[0].products.map(product => (
+                        <OrderCard
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            imageUrl={product.images}
+                            price={product.price}
+                        />
+                    ))
+                }
+            </div>
         </Layout>
     )
 }
