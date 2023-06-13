bafcab0bee8917eaf1b92c714b59cce87dcaf76d
Jamova01 <jorgemova01@gmail.com>
2023-06-13 00:02:06 -0500

    [UPDATE]: Implementing search input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,11 +9,22 @@ import Layout from '../../components/Layout'
 import ProductDetail from '../../components/ProductDetail'
 
 const Home = () => {
-    const { items } = useContext(ShoppingCartContext)
+    const { items, setSearchByTitle } = useContext(ShoppingCartContext)
 
     return (
         <Layout>
-            Home
+            <div className='flex items-center justify-center relative w-80 mb-4'>
+                <h1 className='font-medium text-xl'>Exclusive Products</h1>
+            </div>
+
+            <input
+                type='text'
+                name=''
+                id=''
+                placeholder='Search a product'
+                className='rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none'
+                onChange={(event) => setSearchByTitle(event.target.value)} />
+
             <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
                 {
                     items?.map(product => (
