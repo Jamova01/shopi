f34c3a4052003b73bc93835cc1a3f638cc67dcbb
Jamova01 <jorgemova01@gmail.com>
2023-06-12 20:29:40 -0500

    [UPDATE]: Implementing /my-orders route

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 
 // Context
 import { ShoppingCartContext } from '../../context'
@@ -7,12 +8,20 @@ import { ShoppingCartContext } from '../../context'
 import Layout from '../../components/Layout'
 import OrderCard from '../../components/OrderCard'
 
+// Icons
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
+
 function MyOrder() {
     const { order } = useContext(ShoppingCartContext)
 
     return (
         <Layout>
-            MyOrder
+            <div className='flex items-center justify-center relative w-80 mb-5'>
+                <Link to='/my-orders' className='absolute left-0'>
+                    <ChevronLeftIcon className='h-6 w-6 text-black cursor-pointer' />
+                </Link>
+                <h1>My Order</h1>
+            </div>
             <div className='flex flex-col w-80 gap-4'>
                 {
                     order?.slice(-1)[0]?.products?.map(product => (
