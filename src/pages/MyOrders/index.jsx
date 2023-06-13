1737e1e41bffed0c869408a78b61f45bd35dd227
Jamova01 <jorgemova01@gmail.com>
2023-06-12 20:30:22 -0500

    [UPDATE]: Implement order list

--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -1,9 +1,28 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+
+// Context
+import { ShoppingCartContext } from '../../context/'
+
+// Components
 import Layout from '../../components/Layout'
+import OrdersCard from '../../components/OrdersCard'
 
 function MyOrders() {
+    const { order } = useContext(ShoppingCartContext)
+
     return (
         <Layout>
-            MyOrders
+            <div className='flex items-center justify-center relative w-80'>
+                <h1>My Orders</h1>
+            </div>
+            {
+                order.map((order) => (
+                    <Link key={order.id} to={`/my-orders/${order.id}`}>
+                        <OrdersCard totalPrice={order.totaPrice} totalProducts={order.totalProducts} />
+                    </Link>
+                ))
+            }
         </Layout>
     )
 }
