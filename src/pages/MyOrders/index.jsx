8efb295fb9b43ca3a79a4341c2f92ef669b32285
Jamova01 <jorgemova01@gmail.com>
2023-06-13 00:20:47 -0500

    [UPDATE]: Title styles

--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -13,13 +13,13 @@ function MyOrders() {
 
     return (
         <Layout>
-            <div className='flex items-center justify-center relative w-80'>
-                <h1>My Orders</h1>
+            <div className='flex items-center justify-center relative w-80 mb-4'>
+                <h1 className='font-medium text-xl'>My Orders</h1>
             </div>
             {
                 order.map((order, index) => (
                     <Link key={index} to={`/my-orders/${index}`}>
-                        <OrdersCard totalPrice={order.totaPrice} totalProducts={order.totalProducts} />
+                        <OrdersCard totalPrice={order.totalPrice} totalProducts={order.totalProducts} />
                     </Link>
                 ))
             }
